95c2b8c5dc6ac12814260a6888b785fa32fdef03
Rafael-Santos2 <rafael.araujo03.07.07@gmail.com>
2025-04-14 11:15:04 -0300

    Update perfil_user.php

--- perfil_user.php
+++ perfil_user.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADM - Locadora de veículos</title>
+    <title>USER - Locadora de veículos</title>
     <!-- Link do bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Link dos ícones -->
@@ -30,7 +30,7 @@
                         </span>
                         <!-- Bem vindo,[usuário] -->
                         <span class="welcome-text">
-                            Bem-vindo, <strong>Administrador</strong>!
+                            Bem-vindo, <strong>Usuário</strong>!
                         </span>
                         <!-- botão de logout -->
                         <a href="#" class="btn btn-outline-danger d-flex align-items-center gap-1">
